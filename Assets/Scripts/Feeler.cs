79f0cdbbb4ea81db5a7ea2356cf3308c36b71176
Charles Kelley <Charlesk87@gmail.com>
2014-05-29 23:46:30 -0400

    Add borders and collision detecting for gems
    Added new objects attached to gems to check if there are gems next to
    it, also added borders for the bounds of the gems.

---
+++ Assets/Scripts/Feeler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Feeler : MonoBehaviour {
+
+	public Gem owner;
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.tag == "Gem") 
+		{
+			owner.AddNeighbor(c.GetComponent<Gem>());
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D c)
+	{
+		if (c.tag == "Gem") 
+		{
+			owner.AddNeighbor(c.GetComponent<Gem>());
+		}
+	}
+}
---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Gem : MonoBehaviour {
+
+
+	public List<Gem> Neighbors = new List<Gem>();
+
+	public int GridWidth;
+	public int GridHeight;
+	public GameObject gemPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		print("Clicked");
+	}
+
+	public void AddNeighbor(Gem g)
+	{
+		Neighbors.Add(g);
+	}
+
+	public void RemoveNeighbor(Gem g)
+	{
+		Neighbors.Remove(g);
+	}
+}
