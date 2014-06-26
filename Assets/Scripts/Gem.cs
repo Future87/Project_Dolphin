9ae97fc6722f6d6542ed621f3c85f6b6c194f463
Culinary <brett61188@gmail.com>
2014-06-25 23:26:46 -0400

    Gem color randomizer
    Added functionality to the GEM script to randomize gem color.

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 
 public class Gem : MonoBehaviour {
 
-
+	public string[] gemSpriteName = {"RedGem", "BlueGem", "YellowGem", "GreenGem", "PurpleGem"};
+	string color = "";
+	public GameObject gemShade;
 	public List<Gem> Neighbors = new List<Gem>();
 
 	public int GridWidth;
@@ -13,12 +15,21 @@ public class Gem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		CreateGem ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void CreateGem()
+	{
+		color = gemSpriteName[Random.Range (0,gemSpriteName.Length)];
+		Sprite gemColor = Resources.Load<Sprite> ("Sprites/" + color);
+		gemShade.AddComponent<SpriteRenderer>();
+		gemShade.GetComponent<SpriteRenderer>().sprite = gemColor;
 	}
 
 	void OnMouseDown()
