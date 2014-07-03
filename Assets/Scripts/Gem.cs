fbfa2d779e2094e3fb0206343f6d10275b743585
Culinary <brett61188@gmail.com>
2014-07-02 23:13:19 -0400

    Added particle effects to clicked gems.

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 public class Gem : MonoBehaviour {
 
 	public string[] gemSpriteName = {"RedGem", "BlueGem", "YellowGem", "GreenGem", "PurpleGem"};
+	public GameObject selector;
 	string color = "";
 	public GameObject gemShade;
 	public List<Gem> Neighbors = new List<Gem>();
+	public bool isSelected;
 
 	//public int GridWidth;
 	//public int GridHeight;
@@ -23,7 +25,11 @@ public class Gem : MonoBehaviour {
 	void Update () {
 	
 	}
-
+	public void ToggleSelector()
+	{
+		isSelected = !isSelected;
+		selector.SetActive (isSelected);
+	}
 	public void CreateGem()
 	{
 		color = gemSpriteName[Random.Range (0,gemSpriteName.Length)];
@@ -34,7 +40,7 @@ public class Gem : MonoBehaviour {
 
 	void OnMouseDown()
 	{
-		print("Clicked");
+		ToggleSelector ();
 	}
 
 	public void AddNeighbor(Gem g)
