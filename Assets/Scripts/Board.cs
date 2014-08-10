c0f8fb8357b9f6870ca4bf146faef8a3f4cc328e
Charles Kelley <Charlesk87@gmail.com>
2014-08-10 11:05:51 -0400

    Completed no matches available logic

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -10,6 +10,7 @@ public class Board : MonoBehaviour {
 	public GameObject gemPrefab;
 	public Gem lastGem;
 	public Vector3 gem1Start,gem1End,gem2Start,gem2End;
+	public bool SwapBack = false;
 	public bool isSwapping = false;
 	public Gem gem1, gem2;
 	public float startTime;
@@ -59,13 +60,20 @@ public class Board : MonoBehaviour {
 			{
 				gem1.transform.position = gem1End;
 				gem2.transform.position = gem2End;
-				gem1.ToggleSelector();
-				gem2.ToggleSelector();
 				lastGem = null;
 				
 				isSwapping = false;
 				TogglePhysics(false);
-				CheckMatch();
+				if(!SwapBack)
+				{
+					gem1.ToggleSelector();
+					gem2.ToggleSelector();
+					CheckMatch();
+				}
+				else
+				{
+					SwapBack = false;
+				}
 			}
 		}
 		else if(!DetermineBoardState())
@@ -73,6 +81,15 @@ public class Board : MonoBehaviour {
 			for (int i =0; i < gems.Count; i++)
 			{
 				CheckForNearbyMatches(gems[i]);
+			}
+			
+			if (!DoesBoardContainMathces())
+			{
+				isMatched = true;
+				for (int i =0; i <gems.Count;i++)
+				{
+					gems[i].isMatched = true;
+				}
 			}
 		}
 	}
@@ -98,6 +115,25 @@ public class Board : MonoBehaviour {
 		FixMatchList(gem1,gem1List);
 		ConstructMatchList(gem2.color, gem2, gem2.XCoord, gem2.YCoord,ref gem2List);
 		FixMatchList(gem2, gem2List);
+		if(!isMatched)
+		{
+			SwapBack = true;
+			ResetGems();
+		}
+	}
+	
+	public void ResetGems()
+	{
+		gem1Start = gem1.transform.position;
+		gem1End = gem2.transform.position;
+		
+		gem2Start = gem2.transform.position;
+		gem2End = gem1.transform.position;
+		
+		startTime = Time.time;
+		TogglePhysics(true);
+		
+		isSwapping = true;
 	}
 	public void CheckForNearbyMatches(Gem g)
 	{
@@ -137,6 +173,60 @@ public class Board : MonoBehaviour {
 		}
 	}
 	
+	public bool DoesBoardContainMathces()
+	{
+		TogglePhysics(true);
+		for (int i =0; i < gems.Count; i++)
+		{
+			for (int j = 0; j < gems.Count; j++)
+			{
+				if(gems[i].IsNeighborWith(gems[j]))
+				{
+					Gem g = gems[i];
+					Gem f = gems[j];
+					
+					Vector3 GTemp = g.transform.position;
+					Vector3 FTemp = f.transform.position;
+					List<Gem> tempNeighbors = new List<Gem>(g.Neighbors);
+					
+					g.transform.position = FTemp;
+					f.transform.position = GTemp;
+					g.Neighbors = f.Neighbors;
+					f.Neighbors = tempNeighbors;
+					List<Gem> testListG = new List<Gem>();
+					ConstructMatchList(g.color,g,g.XCoord,g.YCoord, ref testListG);
+					if(TestMatchList(g,testListG))
+					{
+						g.transform.position = GTemp;
+						f.transform.position = FTemp;
+						f.Neighbors = g.Neighbors;
+						g.Neighbors = tempNeighbors;
+						TogglePhysics(false);
+						return true;
+					}
+					List<Gem> testListF = new List<Gem>();
+					ConstructMatchList(f.color,f,f.XCoord,f.YCoord, ref testListF);
+					if (TestMatchList(f, testListF))
+					{
+						g.transform.position = GTemp;
+						f.transform.position = FTemp;
+						f.Neighbors = g.Neighbors;
+						g.Neighbors = tempNeighbors;
+						TogglePhysics(false);
+						return true;
+					}
+					
+					g.transform.position = GTemp;
+					f.transform.position = FTemp;
+					f.Neighbors = g.Neighbors;
+					g.Neighbors = tempNeighbors;
+					TogglePhysics(true);
+				}
+			}
+		}
+		return false;
+	}
+	
 	public void FixMatchList(Gem gem, List<Gem> ListToFix)
 	{
 		List<Gem> rows = new List<Gem>();
@@ -172,6 +262,37 @@ public class Board : MonoBehaviour {
 				columns[i].isMatched = true;
 			}
 		}
+	}
+	
+	public bool TestMatchList(Gem gem, List<Gem> ListToFix)
+	{
+		List<Gem> rows = new List<Gem>();
+		List<Gem> columns = new List<Gem>();
+		
+		for (int i=0; i < ListToFix.Count;i++)
+		{
+			if(gem.XCoord == ListToFix[i].XCoord)
+			{
+				rows.Add(ListToFix[i]);
+			}
+			
+			if(gem.YCoord == ListToFix[i].YCoord)
+			{
+				columns.Add(ListToFix[i]);
+			}
+		}
+		
+		if(rows.Count >= AmountToMatch)
+		{
+			return true;
+		}
+		
+		if(columns.Count >= AmountToMatch)
+		{
+			return true;
+		}
+		
+		return false;
 	}
 	
 	// add in additional parameter over creating a new function, isNegGem should be passed as false when gem is not neg, and true if it is neg.
